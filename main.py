af70544ddcd7968bd1d7e2007ebffff9df84ba06
raghu0309 <raghuramreddy0309@gamil.com>
2024-03-23 16:23:17 +0530

    error handling update

--- main.py
+++ main.py
@@ -1,50 +1,46 @@
-# This is a sample Python script.
+try:
+    def remq(a, p):
+        a.pop(p + 1)
+        a.pop(p)
+        a.pop(p - 1)
+        return a
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+    def calculator(*args):
+        a = []
+        k = 0
+        for i in range(len(s)):
+            if not s[i].isnumeric():
+                a.append(str(s[k:i]))
+                a.append(str(s[i]))
+                k = i + 1
+            if i == len(s) - 1:
+                a.append(str(s[k:]))
 
+        while "*" in a:
+            p = a.index("*")
+            res = float(a[p - 1]) * float(a[p + 1])
+            a = remq(a, p)
+        while "/" in a:
+            p = a.index("/")
+            res = float(a[p - 1]) / float(a[p + 1])
+            a = remq(a, p)
+            a.insert(p - 1, res)
+        res = float(a[0])
+        for i in range(len(a)):
+            if a[i] == "+":
+                res += float(a[i + 1])
+            if a[i] == "-":
+                res -= float(a[i + 1])
+        return res
+    s=input("Enter Operation Format With [+,*,-,+] Only\n")
+    print(calculator(s))
+except :
+    print("Enter Valid in Format Example:  1+2*3/4\n")
 
+else:
+    print(s, end=" = ")
 
-def remq(a,p):
-    a.pop(p+1)
-    a.pop(p)
-    a.pop(p-1)
-    return a
-def calculator(*args):
-    a = []
-    k = 0
-    for i in range(len(s)):
-        if not s[i].isnumeric():
-            a.append(str(s[k:i]))
-            a.append(str(s[i]))
-            k = i + 1
-        if i == len(s) - 1:
-            a.append(str(s[k:]))
-
-    while "*" in a:
-        p = a.index("*")
-        res = float(a[p - 1]) * float(a[p + 1])
-        a = remq(a, p)
-        a.insert(p - 1, res)
-        print(res, a)
-    while "/" in a:
-        p = a.index("/")
-        res = float(a[p - 1]) / float(a[p + 1])
-        print(float(a[p - 1]) % float(a[p + 1]))
-        a = remq(a, p)
-        a.insert(p - 1, res)
-        print(res, a)
-    res = float(a[0])
-    for i in range(len(a)):
-        if a[i] == "+":
-            res += float(a[i + 1])
-        if a[i] == "-":
-            res -= float(a[i + 1])
-    return res
-s=input()
-print(s,end=" = ")
-print(calculator(s))
 
 
 
