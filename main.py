08882c70e06d1db3073fcf0d789cf904c7f55e9d
raghu0309 <raghuramreddy0309@gamil.com>
2024-03-30 13:23:25 +0530

    added recursion on wrong input

--- main.py
+++ main.py
@@ -1,51 +1,49 @@
-try:
-    def remq(a, p):
-        a.pop(p + 1)
-        a.pop(p)
-        a.pop(p - 1)
-        return a
+git
+def p1():
+    try:
+        def remq(a, p):
+            a.pop(p + 1)
+            a.pop(p)
+            a.pop(p - 1)
+            return a
 
+        def calculator(*args):
+            a = []
+            k = 0
+            for i in range(len(s)):
+                if not s[i].isnumeric():
+                    a.append(str(s[k:i]))
+                    a.append(str(s[i]))
+                    k = i + 1
+                if i == len(s) - 1:
+                    a.append(str(s[k:]))
 
-    def calculator(*args):
-        a = []
-        k = 0
-        for i in range(len(s)):
-            if not s[i].isnumeric():
-                a.append(str(s[k:i]))
-                a.append(str(s[i]))
-                k = i + 1
-            if i == len(s) - 1:
-                a.append(str(s[k:]))
+            while "*" in a:
+                p = a.index("*")
+                res = float(a[p - 1]) * float(a[p + 1])
+                a = remq(a, p)
+            while "/" in a:
+                p = a.index("/")
+                res = float(a[p - 1]) / float(a[p + 1])
+                a = remq(a, p)
+                a.insert(p - 1, res)
+            res = float(a[0])
+            for i in range(len(a)):
+                if a[i] == "+":
+                    res += float(a[i + 1])
+                if a[i] == "-":
+                    res -= float(a[i + 1])
+            return res
 
-        while "*" in a:
-            p = a.index("*")
-            res = float(a[p - 1]) * float(a[p + 1])
-            a = remq(a, p)
-        while "/" in a:
-            p = a.index("/")
-            res = float(a[p - 1]) / float(a[p + 1])
-            a = remq(a, p)
-            a.insert(p - 1, res)
-        res = float(a[0])
-        for i in range(len(a)):
-            if a[i] == "+":
-                res += float(a[i + 1])
-            if a[i] == "-":
-                res -= float(a[i + 1])
-        return res
-    s=input("Enter Operation Format With [+,*,-,+] Only\n")
-    print(calculator(s))
-except :
-    print("Enter Valid in Format Example:  1+2*3/4\n")
+        s = input("Enter Operation Format With [+,*,-,+] Only\n")
+        b=calculator(s)
+    except:
+        print("Enter Valid in Format Example:  1+2*3/4\n")
+        p1()
 
-else:
-    print(s, end=" = ")
+    else:
+        print(s, end=" = ")
+        print(b)
 
-
-
-
-
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+p1()
